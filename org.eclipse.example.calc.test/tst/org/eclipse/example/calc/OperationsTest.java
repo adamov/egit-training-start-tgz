cc99f48794f8146ec09d3f3f0d6a52dd32020b15
Rade Adamov <adamov@swissonline.ch>
2021-01-16 23:58:34 +0100

    added power operation

--- org.eclipse.example.calc.test/tst/org/eclipse/example/calc/OperationsTest.java
+++ org.eclipse.example.calc.test/tst/org/eclipse/example/calc/OperationsTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.fail;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
+import org.eclipse.example.calc.internal.operations.Power;
 import org.eclipse.example.calc.internal.operations.Square;
 import org.junit.After;
 import org.junit.Before;
@@ -30,6 +31,7 @@ public class OperationsTest {
 		new Minus();
 		new Equals();
 		square = new Square();
+		new Power();
 	}
 
 	@After
@@ -54,11 +56,12 @@ public class OperationsTest {
 		assertTrue(Operations.INSTANCE.getOperation("-") instanceof Minus);
 		assertTrue(Operations.INSTANCE.getOperation("=") instanceof Equals);
 		assertTrue(Operations.INSTANCE.getOperation("^2") instanceof Square);
+		assertTrue(Operations.INSTANCE.getOperation("^^") instanceof Power);
 	}
 
 	@Test
 	public void testSize() {
-		assertEquals(4, Operations.INSTANCE.size());
+		assertEquals(5, Operations.INSTANCE.size());
 	}
 
 	@Test
