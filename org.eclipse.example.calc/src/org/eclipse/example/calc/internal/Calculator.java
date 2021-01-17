2b143e0c3f59be778e81431b593e362fce91c240
Rade Adamov <adamov@swissonline.ch>
2021-01-17 15:14:35 +0100

    added version number to widow title

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
 
 	private float value;
 
-	public static String NAME = "Simple Calculator";
+	public static String NAME = "Simple Calculator, 0.1.0";
 
 	public Calculator(TextProvider textProvider) {
 		this.textProvider = textProvider;
